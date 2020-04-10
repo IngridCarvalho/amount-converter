4d8efc93388040d583adc68a1d12035a00d69e9c
Ingrid Carvalho <ingridcarvalho_1007@hotmail.com>
2020-04-10 10:53:46 -0300

    fix: add variables in enviromment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://economia.awesomeapi.com.br/json/all/'
+  apiUrl: 'https://economia.awesomeapi.com.br/json/all/',
+  cardFee: 0.0638,
+  moneyFee: 0.011
 };
